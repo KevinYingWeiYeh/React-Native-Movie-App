7a2915b724813cb54d7a7bc2d03f63372f008186
Kevin Yeh <revinyeh@gmail.com>
2018-04-04 23:08:47 -0700

    fixed homepage with indicator when it is loading + changed homepage data display

--- App.js
+++ App.js
@@ -10,12 +10,73 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableHighlight
+  TouchableHighlight,
+  ActivityIndicator,
+  Image,
+  ListView,
 } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state ={ 
+      isLoading: true,
+      mainButton: 'Now Playing',
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    }
+  }
+
+  componentDidMount(){
+    this.movieFetch('Now Playing',1)
+  }
+
+  movieFetch(mainButton, page) {
+    return fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=cc79bee81cab976b941237e667cd8bdd&language=en-US&page=' + page)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: this.state.dataSource
+            .cloneWithRows(responseJson.results)
+      })
+    })
+  }
+
+  renderRow(item) {
+    return (
+      <View style={styles.feed}>
+          <Image source={{uri: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + item.poster_path }} style={{ height: 48, width: 48,}} /> 
+          <View style={{
+            paddingLeft: 5
+          }}>
+          <Text style={styles.feedText}>
+            {item.title}
+          </Text>
+          <Text >
+            Popularity: {Math.round(item.popularity)}
+          </Text>
+          </View>
+      </View>
+    )
+  }
+
   render() {
+    if(this.state.isLoading) {
+      return (
+        <View style={{
+          flex:1,
+          justifyContent: 'center'
+        }}
+        >
+          <ActivityIndicator
+            size='large'
+            animating={true}
+          />
+        </View>
+      )
+    }
     return (
       <View style={styles.container}>
         <TouchableHighlight style={styles.table} >
@@ -28,6 +89,32 @@ export default class App extends Component<Props> {
           Upcoming Movies
           </Text>
         </TouchableHighlight>
+        {/*<FlatList
+          style={{paddingTop: 20, borderColor: '#D7D7D7', borderBottomWidth: 1, marginTop: 5}}
+          data={this.state.dataSource}
+          renderItem={({item}) => 
+            <Text style={styles.feed}> 
+              <Image source={{uri: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + item.poster_path }} style={{ height: 48, width: 48,}} /> 
+              <View style={{
+                paddingLeft: 20
+              }}>
+              <Text style={styles.feedText}>
+                {item.title}
+              </Text>
+              <Text >
+                Popularity: {Math.round(item.popularity)}
+              </Text>
+              </View>
+            </Text>}
+          keyExtractor={(item, index) => index.toString()}
+          />*/}
+          <ListView
+            style={{marginTop: 5}}
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow.bind(this)} />
+        <Text style={{paddingTop: 20}}>
+        end of the Line
+        </Text>
       </View>
     );
   }
@@ -53,4 +140,16 @@ const styles = StyleSheet.create({
     color: '#FFF',
     alignSelf: 'center',
   },
+  feed: {
+    padding : 5,
+    flex:1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#D7D7D7',
+    borderBottomWidth: 1,
+  },
+  feedText: {
+    fontSize: 22,
+    color: '#000',
+  }  
 });
