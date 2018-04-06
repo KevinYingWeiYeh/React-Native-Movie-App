62a0981ad4cc3cd9c1d07c12e3e23186ca4fb1f3
Kevin Yeh <revinyeh@gmail.com>
2018-04-06 11:41:33 -0700

    add pages button and display

--- App.js
+++ App.js
@@ -14,7 +14,10 @@ import {
   ActivityIndicator,
   Image,
   ListView,
+  Navigator,
 } from 'react-native';
+import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
+
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -26,13 +29,15 @@ export default class App extends Component<Props> {
       mainButton: 'Now Playing',
       dataSource: ds.cloneWithRows([]),
       playingPage: 1,
-      upcomingPage: 1
+      upcomingPage: 1,
+      playingTotalPage: 1
     }
-    this.showButton = this.showButton.bind(this)
+    this.showButton = this.showButton.bind(this);
+    this.pageChange = this.pageChange.bind(this);
   }
 
   componentDidMount(){
-    this.movieFetch(this.state.mainButton,this.state.playingPage)
+    this.movieFetch(this.state.mainButton,this.state.playingPage);
   }
 
   movieFetch(mainButton, page) {
@@ -49,6 +54,7 @@ export default class App extends Component<Props> {
       })
     })
   }
+
   showButton(button){
     if(button === 'Now Playing') {
       this.setState({mainButton:'Now Playing'}, () => this.movieFetch(this.state.mainButton,1) )
@@ -57,25 +63,35 @@ export default class App extends Component<Props> {
     }
   }
 
+  pageChange(page){
+
+  }
+
   renderRow(item) {
     return (
       <View style={styles.feed}>
           <Image source={{uri: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + item.poster_path }} style={{ height: 48, width: 48,}} /> 
           <View style={{
-            paddingLeft: 5
+            paddingLeft: 5,
+            width: '70%'
           }}>
-          <Text style={styles.feedText}>
-            {item.title}
-          </Text>
-          <Text >
-            Popularity: {Math.round(item.popularity)}
-          </Text>
+            <Text style={styles.feedText}>
+              {item.title}
+            </Text>
+            <Text >
+              <Icon ios='ios-heart' android="md-heart" style={{fontSize: 15, color: 'red'}}/>
+              {Math.round(item.popularity)}
+            </Text>
+          </View>
+          <View>
+            <Text style={{textAlign: 'right', color: 'blue'}}> View </Text>
           </View>
       </View>
     )
   }
 
   render() {
+
     if(this.state.isLoading) {
       return (
         <View style={{
@@ -110,27 +126,27 @@ export default class App extends Component<Props> {
           Upcoming Movies
           </Text>
         </TouchableHighlight>
-          <ListView
-            style={this.state.mainButton === 'Now Playing' ? {display: 'flex'} : {display: 'none'}}
-            dataSource={this.state.dataSource}
-            renderRow={this.renderRow.bind(this)} />
-          <ListView
-            style={this.state.mainButton === 'Upcoming Movies' ? {display: 'flex'} : {display: 'none'}}
-            dataSource={this.state.dataSource}
-            renderRow={this.renderRow.bind(this)} />
-          <Text style={{marginTop: 5}}>
-          <View style={styles.button}>
-          <Text style={styles.tableText}>
-            LastPage
-          </Text>
+        <ListView
+          style={{marginTop:5}}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)} />
+          <View style={{flexDirection:'row', flexWrap:'wrap'}}>
+          <TouchableHighlight
+            style={styles.table}
+           >
+            <Text style={styles.buttonText}>
+              LastPage
+            </Text>
+          </TouchableHighlight >
+          <Text style={styles.page}> Page: {this.state.playingPage} / {this.state.playingTotalPage}</Text>
+          <TouchableHighlight
+            style={styles.table}
+           >
+            <Text style={styles.buttonText}>
+              NextPage
+            </Text>
+          </TouchableHighlight >
         </View>
-        <Text>  </Text>
-        <View  style={styles.button}>
-          <Text style={styles.tableText}>
-            NextPage
-          </Text>
-        </View>
-        </Text>
       </View>
     );
   }
@@ -161,7 +177,6 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: '#000',
     alignSelf: 'center',
-
   },
   feed: {
     padding : 5,
@@ -174,6 +189,7 @@ const styles = StyleSheet.create({
   feedText: {
     fontSize: 18,
     color: '#000',
+    paddingBottom: 5,
   },
   button: {
     height: 50,
@@ -181,6 +197,18 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     justifyContent: 'center',
     padding: 10,
-    borderRadius: 18,
-  }
+    borderRadius: 30,
+  },
+  buttonText: {
+    alignSelf: 'stretch',
+    fontSize: 18,
+    color: '#FFF',
+    padding: 10,
+    alignItems: 'center',
+  },
+  page: {
+    alignSelf: 'center',
+    fontSize: 15,
+    padding: 5,
+  },
 });
